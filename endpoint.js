64e6538a71153b701b2062a50fcbad7d77ea09d8
Isaac Tong <isaact943@outlook.com>
2020-05-30 11:59:27 +0800

    before food parsing js file

--- endpoint.js
+++ endpoint.js
@@ -9,43 +9,32 @@ const dbName = 'Food-List'
 
 const client = new MongoClient(url);
 
-MongoClient.connect(url, { useUnifiedTopology: true },  function(error, client) {
-  if(error){
-    return console.log('Failed to connect to databse')
-  }
-
-  const db = client.db(dbName)
-
-  db.collection('foods').insertOne({
-    name: 'apple',
-    type: 'fruit'
-  },
-  (error, result) => {
-    if(error){
-      console.log('Cannot insert');
-    }
-    console.log(result.ops);
-  });
-
-
-
-
-});
-
-
-
-
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
-
-// app.use('/', () => {
-//   console.log('Posted');
-// })
+// MongoClient.connect(url, { useUnifiedTopology: true },  function(error, client) {
+//   if(error){
+//     return console.log('Failed to connect to databse')
+//   }
+//   const db = client.db(dbName)
+//   db.collection('foods').insertOne({
+//     name: 'apple',
+//     type: 'fruit'
+//   },
+//   (error, result) => {
+//     if(error){
+//       console.log('Cannot insert');
+//     }
+//     console.log(result.ops);
+//   });
 //
-// app.get('/', (req, res) => {
-//   res.send('hi')
-// })
-//
-//
-//
-// app.listen(3000);
+// });
+
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json()
+})
+
+
+app.listen(3000);
