fbdb40e8980deccfc631d67e7d388dae072d26c7
Isaac Tong <isaact943@outlook.com>
2020-05-30 11:34:54 +0800

    test add document to mongoDB

--- endpoint.js
+++ endpoint.js
@@ -2,17 +2,50 @@ const express = require('express')
 const app = express()
 require('dotenv').config()
 
+const MongoClient = require('mongodb').MongoClient;
+
+const url = process.env.DB_STRING
+const dbName = 'Food-List'
+
+const client = new MongoClient(url);
+
+MongoClient.connect(url, { useUnifiedTopology: true },  function(error, client) {
+  if(error){
+    return console.log('Failed to connect to databse')
+  }
+
+  const db = client.db(dbName)
+
+  db.collection('foods').insertOne({
+    name: 'apple',
+    type: 'fruit'
+  },
+  (error, result) => {
+    if(error){
+      console.log('Cannot insert');
+    }
+    console.log(result.ops);
+  });
+
+
+
+
+});
+
+
+
+
 // app.use(express.urlencoded({ extended: true }));
 // app.use(express.json());
 
-app.use('/', () => {
-  console.log('Posted');
-})
-
-app.get('/', (req, res) => {
-  res.send('hi')
-})
-
-
-
-app.listen(3000);
+// app.use('/', () => {
+//   console.log('Posted');
+// })
+//
+// app.get('/', (req, res) => {
+//   res.send('hi')
+// })
+//
+//
+//
+// app.listen(3000);
